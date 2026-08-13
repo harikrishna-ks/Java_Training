c90612cac027174667136f35857ef51e2655f906
HARIKRISHNA K S <ksharikrishna96@gmail.com>
2026-08-13 09:12:35 +0530

    Uncomment code to filter zeros from array

--- LastZero.java
+++ LastZero.java
@@ -1,20 +1,20 @@
 import java.util.Arrays;
 public class LastZero {
     public static void main(String[] args) {
-        // int arr[]={1,0,2,0,3,0};
-        // int index=0;
-        // for(int i=0;i<arr.length;i++){
-        //     if(arr[i]!=0){
-        //         arr[index]=arr[i];
-        //         index++;
-        //     }
-        // }
-        // System.out.println(Arrays.toString(arr));
-        // while(index<arr.length){
-        //     arr[index]=0;
-        //     index++;
-        // }
-        // System.out.print(Arrays.toString(arr));
+        int arr[]={1,0,2,0,3,0};
+        int index=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=0){
+                arr[index]=arr[i];
+                index++;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+        while(index<arr.length){
+            arr[index]=0;
+            index++;
+        }
+        System.out.print(Arrays.toString(arr));
         //ip 
         //op {1,2,3,a,b,c}
             char arr[]={'a', '1', 'b', '2', 'c', '3'};
@@ -24,8 +24,6 @@ public class LastZero {
                     arr[index]=arr[i];
                     index++;
                 }
-            
-
             }
             System.out.println(Arrays.toString(arr));
 
