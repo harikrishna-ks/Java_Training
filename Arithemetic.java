ef5873e775e213ba9b3a7ddc976a4002e30fbe98
HARIKRISHNA K S <ksharikrishna96@gmail.com>
2026-08-13 09:09:26 +0530

    Uncomment arithmetic and Fibonacci operations

--- Arithemetic.java
+++ Arithemetic.java
@@ -1,25 +1,25 @@
 public class Arithemetic {
     public static void main(String[] args) {
-        // int a=5;
-        // int b=10;
-        // System.out.println(a+b);
-        // System.out.println(a-b);
-        // System.out.println(a*b);    
-        // System.out.println(a/b);
-        // System.out.println(a%b);
+        int a=5;
+        int b=10;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);    
+        System.out.println(a/b);
+        System.out.println(a%b);
         
-            // Fibonacci series
+            Fibonacci series
 
-        // int n=5;
-        // int a=0;
-        // int b=1;
-        // System.out.print(a+" "+b);
-        // for(int i=3;i<=n;i++){
-        //     int c=a+b;
-        //     a=b;
-        //     b=c;
-        //     System.out.print(" "+c);
-        // }
+        int n=5;
+        int a=0;
+        int b=1;
+        System.out.print(a+" "+b);
+        for(int i=3;i<=n;i++){
+            int c=a+b;
+            a=b;
+            b=c;
+            System.out.print(" "+c);
+        }
 
         
 
